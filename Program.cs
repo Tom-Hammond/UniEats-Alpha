4bfb914f00965bbea35a8c1ecd74c084730bb1be
TOM HAMMOND <T.J.HAMMOND-2021@hull.ac.uk>
2023-02-21 20:33:40 +0000

    Able to read in user details from a csv file

--- Program.cs
+++ Program.cs
@@ -3,20 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Text;
+
 
 namespace UniEats_Alpha
 {
+  
     static class Program
     {
+
+        public static List<string> userName  = new List<string>();
+        public static List<string> userPassword = new List<string>();
+        public static List<string> userType = new List<string>();
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+
+          
+
+            using (var reader = new StreamReader("Users.csv"))
+            {
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();   //Reads each line of file 
+                    var values = line.Split(',', ',');   //Splits the colums at the comma 
+
+                    userName.Add(values[0]);       //Adds the values of the username colum to the list 
+                    userPassword.Add(values[1]);          //Adds the values of the password colum to the list
+                    userType.Add(values[2]);             //Adds the values of the user type colum to the list
+
+
+                }
+
+               
+
+            }
+
+           
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new HomePage());
         }
+
+        
     }
 }
