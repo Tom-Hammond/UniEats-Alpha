6f3f9836a9d3281029ef66fcefeb41e9adb203df
TOM HAMMOND <T.J.HAMMOND-2021@hull.ac.uk>
2023-02-22 00:51:13 +0000

    Created a new shop form so user can see menu and shop details

---
+++ SparShop.Designer.cs
@@ -0,0 +1,50 @@
+﻿
+namespace UniEats_Alpha
+{
+    partial class SparShop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // SparShop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::UniEats_Alpha.Properties.Resources.IphoneHomePage;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(642, 1029);
+            this.DoubleBuffered = true;
+            this.Name = "SparShop";
+            this.Text = "SparShop";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
